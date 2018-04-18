82838f19a5bcc179c037e229bb29da64202eb9c9
b23 <b23@B23-04>
2018-04-18 11:43:26 +0200

    Konec vaje

--- VajaMVC/VajaMVC/Controllers/StudentController.cs
+++ VajaMVC/VajaMVC/Controllers/StudentController.cs
@@ -10,9 +10,8 @@ namespace VajaMVC.Controllers
     public class StudentController : Controller
     {
         // GET: Student
-        public ActionResult Index()
-        {
-            var studentList = new List<Student>{
+        
+            List<Student> studentList = new List<Student>{
              new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
              new Student() { StudentId = 2, StudentName = "Steve", Age = 21 } ,
              new Student() { StudentId = 3, StudentName = "Bill", Age = 25 } ,
@@ -20,8 +19,26 @@ namespace VajaMVC.Controllers
              new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
              new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
              new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
-            };
+            };
+
+        public ActionResult Index()
+        {
             return View(studentList);
+        }
+        public ActionResult Edit(int id)
+        {
+            var študent = studentList.Where(s => s.StudentId == id).FirstOrDefault();
+            return View(študent);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                //posodobi podatke
+                return RedirectToAction("Index");
+            }
+            return View(std);
         }
     }
 }
